a00973d3d07b09121bfd2b28cfdc25cc58ce842d
REXXAR-677 <ricardovera71@gmail.com>
2022-06-04 20:41:04 -0400

    build(config): added testing config and storybook

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,11 @@
 import type { NextPage } from 'next';
+
 const Home: NextPage = () => {
-  return <div>App</div>;
+  return (
+    <div>
+      <h1 data-testid='home' className="text-3xl font-bold underline ">Hi mom!</h1>
+    </div>
+  );
 };
 
 export default Home;
