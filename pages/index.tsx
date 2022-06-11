8391159de1ef7c2cb49e80566a95e754c05e4fa6
jhonderquintero <jhonderquintero@gmail.com>
2022-06-11 19:13:36 -0400

    build(fix): Fixing configuration files

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,9 +3,7 @@ import type { NextPage } from 'next';
 const Home: NextPage = () => {
   return (
     <div>
-      <h1 data-testid="home" className="text-3xl font-bold underline ">
-        Hi mom!
-      </h1>
+      <h1 className="bg-slate-900	text-white">Homepage</h1>
     </div>
   );
 };
