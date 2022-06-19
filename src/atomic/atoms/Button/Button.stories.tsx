f731cc18103fe200218ed03faa2885ac2213ca67
jhonderquintero <jhonderquintero@gmail.com>
2022-06-19 15:32:17 -0400

    add(component): New Component Button

--- src/atomic/atoms/Button/Button.stories.tsx
+++ src/atomic/atoms/Button/Button.stories.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { ComponentStory, ComponentMeta } from '@storybook/react';
+import './../../../styles/globals.css';
 
 import { Button } from './Button';
 
@@ -14,28 +15,22 @@ export default {
 } as ComponentMeta<typeof Button>;
 
 // More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
-const Template: ComponentStory<typeof Button> = (args) => <Button {...args} />;
+const Template: ComponentStory<typeof Button> = (args) => (
+  <Button {...args}>Button</Button>
+);
 
-export const Primary = Template.bind({});
+export const Normal = Template.bind({});
 // More on args: https://storybook.js.org/docs/react/writing-stories/args
-Primary.args = {
-  primary: true,
-  label: 'Button',
+Normal.args = {
+  variant: 'normal',
 };
 
-export const Secondary = Template.bind({});
-Secondary.args = {
-  label: 'Button',
+export const Outlined = Template.bind({});
+Outlined.args = {
+  variant: 'outlined',
 };
 
-export const Large = Template.bind({});
-Large.args = {
-  size: 'large',
-  label: 'Button',
-};
-
-export const Small = Template.bind({});
-Small.args = {
-  size: 'small',
-  label: 'Button',
+export const Text = Template.bind({});
+Text.args = {
+  variant: 'text',
 };
