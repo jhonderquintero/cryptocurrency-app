f87a63caf02cc204bfaf128aed83d9b0f2ae77b3
ricardovera76 <ricardovera71@gmail.com>
2022-06-29 19:18:39 -0400

    fix(theme issue): fixed the theme to fit the entire screen

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,16 @@
 import type { NextPage } from 'next';
-import { ThemeProvider } from '../context/ThemeContext';
+import { Button } from '../atomic/atoms/Button/Button';
+import Typography from '../atomic/atoms/Typography/Typography';
+import { ThemeProvider, useThemeProvider } from '../context/ThemeContext';
 
 const Home: NextPage = () => {
+  const ctx = useThemeProvider();
   return (
     <ThemeProvider>
-      <div className="bg-surface	dark:bg-surface-dark"></div>
+      <div>
+        <Typography variant="heading">Change the theme!</Typography>
+        <Button onClick={() => ctx.toggleColorScheme()}>Change</Button>
+      </div>
     </ThemeProvider>
   );
 };
